d8b3476102d39026783521a58de97ad36d8a94f3
debamitra <debamitra.mukherjee@gmail.com>
2019-04-21 05:15:17 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname +'/index.html');
+})
+
+app.get('/login', function(req, res) {
+    res.json({'name':'being zero','age':'2 years'});
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
